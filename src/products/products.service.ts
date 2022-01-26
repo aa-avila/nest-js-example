29a28fa332581cdb89d34b66c4306b32ec6709af
Agustin <agualvila@gmail.com>
2022-01-26 13:38:05 -0300

    added ulidx

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from './product.model';
+import { ulid } from 'ulidx';
 
 @Injectable()
 export class ProductsService {
   products: Product[] = [];
 
   insertProduct(title: string, desc: string, price: number) {
-    const id = Math.random().toString();
+    const id = ulid();
     const newProduct = new Product(id, title, desc, price);
     this.products.push(newProduct);
     return id;
