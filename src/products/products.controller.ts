d37ad0c00f5d1ae8cc4377d832552e322e09986c
Agustin <agualvila@gmail.com>
2022-01-26 15:55:44 -0300

    complete basic products CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -17,5 +25,35 @@ export class ProductsController {
       prodPrice,
     );
     return { id: prodId };
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+
+  @Get(':id')
+  getProductById(@Param('id') prodId: string) {
+    return this.productsService.getProductById(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('desc') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    return this.productsService.updateProduct(
+      prodId,
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') prodId: string) {
+    return this.productsService.deleteProduct(prodId);
   }
 }
